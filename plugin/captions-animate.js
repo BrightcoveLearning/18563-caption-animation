932291355230fcc77f4bdd06e8f8f54cd96bbdf4
Jeff Doktor <jdoktor@brightcove.com>
2018-03-14 08:51:46 -0600

    initial code

---
+++ plugin/captions-animate.js
@@ -0,0 +1,20 @@
+videojs.registerPlugin('captionsAnimate', function() {
+    var myPlayer = this;
+
+    // Optional muting of the player for testing
+    myPlayer.muted(true);
+
+    // Wait till player metadata has loaded
+    myPlayer.on("loadedmetadata", function () {
+      var lang="en";
+      // Open the English version captions
+      for (var i = 0; i < (myPlayer.textTracks().length); i++) {
+        if (myPlayer.textTracks()[i].language == lang) {
+          // console.log(lang);
+          // console.log(myPlayer.textTracks()[i].language);
+          myPlayer.textTracks()[i].mode = "showing";
+        }
+      }
+    });
+
+});
